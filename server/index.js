e13d1789337f2a05fe861cf4c3be2d9f2e7c2332
Adam Londero <alondero@gmail.com>
2016-03-26 16:36:23 +0000

    Moving messages to loki to allow new messages to be added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var app = express();
+var loki = require('lokijs')
+var db = new loki('messages.json', {
+        autosave: true, 
+        autosaveInterval: 10000,
+        autoload: true, 
+        autoloadCallback: loadHandler
+});
+var messages;
 
-var messages = ['Hello World!'];
+function loadHandler() {
+  messages = db.getCollection('messages');
+  if (!messages) {
+    messages = db.addCollection('messages');
+    messages.insert( { message:'Hello World!' } );
+    db.saveDatabase()
+  }
+}
 
+// Gets most recent message
 app.get('/', function (req, res) {
-  var message = messages[0];
-  res.json({'message':message});
+  res.json({'message':messages.get(messages.data.length).message});
 });
 
 module.exports = app;
